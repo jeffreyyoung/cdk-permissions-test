e0ac2d2e781be9d196740ed73fea91d997d7ad92
Jeffrey Young <jeffrey@macbook.com>
2023-04-19 17:09:48 -0700

    add website

--- lib/cdk-permissions-test-stack.ts
+++ lib/cdk-permissions-test-stack.ts
@@ -1,16 +1,37 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkPermissionsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an S3 bucket to host the website
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+      publicReadAccess: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPermissionsTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Deploy the index.html file to the bucket
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('./website')],
+      destinationBucket: websiteBucket,
+    });
+
+    // Create a CloudFront distribution to serve the website
+    const distribution = new cloudfront.Distribution(this, 'Distribution', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    // Output the website URL
+    new cdk.CfnOutput(this, 'WebsiteUrl', {
+      value: `https://${distribution.distributionDomainName}`,
+    });
   }
 }
