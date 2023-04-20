757ba745cbe8c338e44966a5a797e52e53901adf
Jeffrey Young <jeffrey@macbook.com>
2023-04-20 01:55:39 -0700

    omg it works

--- lib/cdk-permissions-test-stack.ts
+++ lib/cdk-permissions-test-stack.ts
@@ -3,10 +3,9 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as cloudfront_origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// https://aws-cdk.com/deploying-a-static-website-using-s3-and-cloudfront
 
+// https://aws-cdk.com/deploying-a-static-website-using-s3-and-cloudfront
 export class CdkPermissionsTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
