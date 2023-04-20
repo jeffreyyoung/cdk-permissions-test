96e8bba16b0c07f1aa907664473953d6c3ad9fee
Jeffrey Young <jeffrey@macbook.com>
2023-04-19 17:54:29 -0700

    simplify app

--- lib/cdk-permissions-test-stack.ts
+++ lib/cdk-permissions-test-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
 
 export class CdkPermissionsTestStack extends cdk.Stack {
@@ -19,19 +17,6 @@ export class CdkPermissionsTestStack extends cdk.Stack {
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
       sources: [s3deploy.Source.asset('./website')],
       destinationBucket: websiteBucket,
-    });
-
-    // Create a CloudFront distribution to serve the website
-    const distribution = new cloudfront.Distribution(this, 'Distribution', {
-      defaultBehavior: {
-        origin: new origins.S3Origin(websiteBucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      },
-    });
-
-    // Output the website URL
-    new cdk.CfnOutput(this, 'WebsiteUrl', {
-      value: `https://${distribution.distributionDomainName}`,
     });
   }
 }
